ea97087f587898c9ac3d337f1f756b1017e63dfe
anjary-05 <99781214+anjary-05@users.noreply.github.com>
2023-05-08 13:18:26 +0300

    Update accueilIA.blade.php

--- web-design/resources/views/Front-Office/accueilIA.blade.php
+++ web-design/resources/views/Front-Office/accueilIA.blade.php
@@ -48,12 +48,13 @@
         </section>
         -->
 
-        <!-- Actions -->
+        <!-- Actions 
         <section>
             <ul class="actions stacked">
-                <li><a href="#" class="button large fit">quitter</a></li>
+                <li><a href="#" rel="noopener" target="_blank" class="button large fit">quitter</a></li>
             </ul>
         </section>
+        -->
 
     </section>
 
@@ -66,24 +67,24 @@
         <article class="post">
             <header>
                 <div class="title">
-                    <h2><a href="single.html">{{$data2->titre}}</a></h2>
+                    <h2><a rel="noopener" target="_blank" href="#">{{$data2->titre}}</a></h2>
                    <!-- <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>-->
                 </div>
                 <div class="meta">
                     <time class="published" >{{ $function->date_inword($data2->date_sortie) }}</time>
-                    <a href="#" class="author"><span class="name">{{ $data2->auteur }}</span><img src="images/avatar.jpg" alt="" /></a>
+                    <a href="#" rel="noopener" target="_blank" class="author"><span class="name">{{ $data2->auteur }}</span><img src="images/avatar.jpg" alt="" /></a>
                 </div>
             </header>
-            <a href="single.html" class="image featured"><img src="data:image/{{ $data2->extension }};base64,{{ $data2->image }}" alt="" /></a>
+            <a href="#" rel="noopener" target="_blank" class="image featured"><img src="data:image/{{ $data2->extension }};base64,{{ $data2->image }}" alt="" /></a>
             {!!  $data2->contenu !!}
             <footer>
                 <ul class="actions">
                     <li><a href="{{ route('detail', ['id' => md5($data2->id)]) }}" class="button large">voir en detail</a></li>
                 </ul>
                 <ul class="stats">
-                    <li><a href="#">General</a></li>
-                    <li><a href="#" class="icon solid fa-heart">28</a></li>
-                    <li><a href="#" class="icon solid fa-comment">128</a></li>
+                    <li><a href="#" rel="noopener" target="_blank">General</a></li>
+                    <li><a href="#" rel="noopener" target="_blank" class="icon solid fa-heart">28</a></li>
+                    <li><a href="#" rel="noopener" target="_blank" class="icon solid fa-comment">128</a></li>
                 </ul>
             </footer>
         </article>
@@ -121,19 +122,19 @@
                 @foreach($data as $data1)
                 <article class="mini-post">
                     <header>
-                        <h3><a href="single.html">{{ $data1->titre }}</a></h3>
+                        <h3><a href="#" rel="noopener" target="_blank" >{{ $data1->titre }}</a></h3>
                         <time class="published" datetime="2015-10-20">{{ $function->date_inword($data1->date_sortie) }} </time>
-                        <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                        <a href="#" rel="noopener" target="_blank" class="author"><img src="images/avatar.jpg" alt="" /></a>
                     </header>
-                    <a href="single.html" class="image"><img src="data:image/{{ $data1->extension }};base64,{{ $data1->image }}" alt="" /></a>
+                    <a href="#" rel="noopener" target="_blank" class="image"><img src="data:image/{{ $data1->extension }};base64,{{ $data1->image }}" alt="" /></a>
                 </article>
                @endforeach
                 @else
                     <section class="blurb">
-                        <h2>data</h2>
-                        <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.</p>
+                        <h2>donnees</h2>
+                        <p>L'intelligence artificielle (IA) est un domaine de l'informatique qui vise à créer des systèmes capables d'effectuer des tâches qui nécessitent normalement l'intelligence humaine, comme la reconnaissance de la parole, la vision, la prise de décision</p>
                         <ul class="actions">
-                            <li><a href="#" class="button">Learn More</a></li>
+                            <li><a href="#" rel="noopener" target="_blank" class="button">Learn More</a></li>
                         </ul>
                     </section>
                 @endif
@@ -145,18 +146,18 @@
             <h2>à propos</h2>
             <p>Le portail francophone indépendant consacré à l'intelligence artificielle et à la datascience, à destination des chercheurs, étudiants, professionnels et passionnés</p>
             <ul class="actions">
-                <li><a href="#" class="button">savoir plus</a></li>
+                <li><a href="#" rel="noopener" target="_blank" class="button">savoir plus</a></li>
             </ul>
         </section>
 
         <!-- Footer -->
         <section id="footer">
             <ul class="icons">
-                <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-                <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-                <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-                <li><a href="#" class="icon solid fa-rss"><span class="label">RSS</span></a></li>
-                <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+                <li><a href="#" rel="noopener" target="_blank" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" rel="noopener" target="_blank" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                <li><a href="#" rel="noopener" target="_blank" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="#" rel="noopener" target="_blank" class="icon solid fa-rss"><span class="label">RSS</span></a></li>
+                <li><a href="#" rel="noopener" target="_blank" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
             </ul>
             <p class="copyright">&copy; Untitled. Design: <a href="http://html5up.net">HTML5 UP</a>. Images: <a href="http://unsplash.com">Unsplash</a>.</p>
         </section>
